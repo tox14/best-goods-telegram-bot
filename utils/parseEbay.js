87b3cf237cc3fca13256c0e7e632aaab5be32709
Anton <dedul.9@yandex.ru>
2021-08-03 16:18:40 +0300

    Added try/catch to parseEbay functions

--- utils/parseEbay.js
+++ utils/parseEbay.js
@@ -2,35 +2,40 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 module.exports = async () => {
-  const goods = [];
-  const store = "eBay";
+  try {
+    const goods = [];
+    const store = "eBay";
 
-  const response = await axios.get("https://www.ebay.com/globaldeals", {
-    headers: {
-      "Accept-Language": "en-US,en;",
-    },
-  });
-  const $ = cheerio.load(response.data);
-
-  $("[data-listing-id]").each((_, el) => {
-    const url = $(el).find(".dne-itemtile-detail > a").attr("href");
-    const title = $(el).find(".dne-itemtile-title").attr("title");
-    const image =
-      $(el).find("img").attr("data-config-src") ||
-      $(el).find("img").attr("src");
-    const price = $(el).find("span[itemprop='price']").text().split(" ")[1];
-    const discount = $(el)
-      .find(".dne-itemtile-original-price .itemtile-price-bold")
-      .text();
-    goods.push({
-      store,
-      url,
-      title,
-      image,
-      price,
-      discount,
+    const response = await axios.get("https://www.ebay.com/globaldeals", {
+      headers: {
+        "Accept-Language": "en-US,en;",
+      },
     });
-  });
+    const $ = cheerio.load(response.data);
 
-  return goods;
+    $("[data-listing-id]").each((_, el) => {
+      const url = $(el).find(".dne-itemtile-detail > a").attr("href");
+      const title = $(el).find(".dne-itemtile-title").attr("title");
+      const image =
+        $(el).find("img").attr("data-config-src") ||
+        $(el).find("img").attr("src");
+      const price = $(el).find("span[itemprop='price']").text().split(" ")[1];
+      const discount = $(el)
+        .find(".dne-itemtile-original-price .itemtile-price-bold")
+        .text();
+      goods.push({
+        store,
+        url,
+        title,
+        image,
+        price,
+        discount,
+      });
+    });
+
+    return goods;
+  } catch (error) {
+    console.error("eBay parse ERROR: " + error);
+    return [];
+  }
 };
